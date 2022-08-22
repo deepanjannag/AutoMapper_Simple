ccaf44c181ebe7d4c6f71066a657765f5cec7f21
Deepanjan Nag <subscriptions@deepanjannag.com>
2022-08-21 21:49:18 -0400

    Initial commit

---
+++ AutoMapperDemo/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using AutoMapperDemo.Dto;
+using AutoMapperDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AutoMapperDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+
+        public UserController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<UserForUI> ShowUser()
+        {
+            User u = new User { Id=1, FirstName="Jamsetji", LastName="Tata", Address="Jamshedpur", Email= "jtata@tata.com" };
+
+            return _mapper.Map<UserForUI>(u);
+
+        }
+    }
+}
---
+++ AutoMapperDemo/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using AutoMapperDemo.Dto;
+using AutoMapperDemo.Models;
+
+namespace AutoMapperDemo
+{
+    //As soon as our app starts, AutoMapper will scan our app for classes that inherit from Profile class and load their mapping configurations
+    public class UserProfile: Profile       //Profile is an AutoMapper class
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserForUI>();
+        }
+    }
+}
